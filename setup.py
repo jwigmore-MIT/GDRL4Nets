782c9c7ce78378adebd74fd31af73dece405cda4
jwigmore-laptop <jwigmore@mit.edu>
2024-03-08 14:44:45 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GDRL4Nets',
+    version='0.1.0',
+    description='A brief description of my project',
+    packages=find_packages(),
+)
