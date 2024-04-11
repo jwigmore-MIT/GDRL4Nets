7cdbe25b9700b68fef60e49f2729d8aaa1b6c30a
jwigmore-laptop <jwigmore@mit.edu>
2024-04-11 19:09:18 -0400

    For MDP solving

--- MDP_Solver/context_set_to_mdp_script.py
+++ MDP_Solver/context_set_to_mdp_script.py
@@ -4,23 +4,24 @@ from SingleHopMDP import SingleHopMDP
 import json
 import os
 
-max_iterations = 300
+max_iterations = 2
 
 context_set_dict = json.load(open("context_sets/SH1_poisson_context_set.json", 'rb'))
 # for i in range(context_set_dict["num_envs"]):
-for i in [0]:
+for i in [0, 1,2]:
     context_dict = context_set_dict["context_dicts"][str(i)]
     env_params = context_dict["env_params"]
     env = make_env(env_params)
-    mdp = SingleHopMDP(env, name = f"SH1_Poisson_{i}_MDP", q_max = 40)
+    mdp = SingleHopMDP(env, name = f"SH1_Poisson_{i}_MDP", q_max = 50)
     mdp.compute_tx_matrix(save_path = "saved_tx_matrices")
-    mdp.load_tx_matrix("saved_tx_matrices/SH1_1_MDP_qmax60_discount0.99_computed_tx_matrix.pkl")
+    # mdp.load_tx_matrix("saved_tx_matrices/SH1_1_MDP_qmax60_discount0.99_computed_tx_matrix.pkl")
     mdp.do_VI(max_iterations = max_iterations, theta = 0.1)
-    save_path = os.path.join("saved_mdps", f"SH1_{i}_MDP.p")
+
+    save_path = os.path.join("saved_mdps", f"SH1_Poisson_{i}_MDP.p")
     # check if the save_path already exists, if it does add a number to the end of the file name
     if os.path.exists(save_path):
         j = 1
         while os.path.exists(save_path):
-            save_path = os.path.join("saved_mdps", f"SH1_{i}_MDP_{j}.p")
+            save_path = os.path.join("saved_mdps", f"SH1_Poisson_{i}_MDP_{j}.p")
             j += 1
     mdp.save_MDP(save_path)
