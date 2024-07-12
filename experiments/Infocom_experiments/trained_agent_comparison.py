a0f26b8c8642f4dd1b09c76346ec27a1c214a7b1
jwigmore-laptop <jwigmore@mit.edu>
2024-07-12 10:24:29 -0400

    trained_agent_comparison updates

--- experiments/Infocom_experiments/trained_agent_comparison.py
+++ experiments/Infocom_experiments/trained_agent_comparison.py
@@ -13,9 +13,20 @@ import numpy as np
 from torchrl_development.utils.configuration import load_config
 from torchrl_development.actors import create_independent_actor_critic
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser(description='Test Agents')
+# add argument for context set folder
+parser.add_argument('--context_set_folder', type=str, help='folder containing context set', default="n2SH2u")
+# add argument for context set file name
+parser.add_argument('--context_set_file_name', type=str, help='file name of context set', default="n2SH2u_context_set_l1_m3_s30.json")
+# add argument for agent types (list of strings)
+parser.add_argument('--agent_types', nargs='+', type=str, help='list of agent types', default=["MWN", "MLP", "PMN"])
+
+args = parser.parse_args()
+
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(os.path.join(PROJECT_DIR, 'MDP_Solver'))
 
 
 """
@@ -26,52 +37,42 @@ Want to asses the performance of the MWN and MLP agents on all three SH3 context
 
 
 
+context_set_folder = args.context_set_folder
+context_set_file_name = args.context_set_file_name
+agent_types = args.agent_types
 
 
 
-
-rollout_length =30000
+rollout_length = 30000
 num_rollouts = 3
 env_generator_seed = 4162024
 lta_tds = {}
 # test_context_set_path = 'SH2u2_context_set_20_07091947.json'
 #test_context_set_path = "SH3_context_set_100_03251626.json"
-test_context_set_path = "n2SH2u_context_set_l1_m3_s30.json"
+trained_agent_folder = os.path.join("trained_agents",context_set_folder)
+test_context_set_path = os.path.join(trained_agent_folder, context_set_file_name)
 context_set = json.load(open(test_context_set_path, 'rb'))
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-pmn_cfg = load_config(os.path.join(SCRIPT_PATH, 'PMN_Independent_PPO_settings.yaml'))
+pmn_cfg = load_config(os.path.join(SCRIPT_PATH, 'PMN_Shared_PPO_settings.yaml'))
 mlp_cfg = load_config(os.path.join(SCRIPT_PATH, 'MLP_PPO_settings.yaml'))
 
 
-# Create environment generator
-# make_env_parameters = {"graph": getattr(pmn_cfg.training_env, "graph", False),
-#                                     "observe_lambda": getattr(pmn_cfg.training_env, "observe_lambda", True),
-#                                     "terminal_backlog": getattr(pmn_cfg.training_env, "terminal_backlog", None),
-#                                     "observation_keys": getattr(pmn_cfg.training_env, "observation_keys", ["Q", "Y"]),
-#                                     "observation_keys_scale": getattr(pmn_cfg.training_env, "observation_keys_scale", None),
-#                                     "negative_keys": getattr(pmn_cfg.training_env, "negative_keys", ["Y"]),
-#                                     "symlog_obs": getattr(pmn_cfg.training_env, "symlog_obs", False),
-#                                     "symlog_reward": getattr(pmn_cfg.training_env, "symlog_reward", False),
-#                                     "inverse_reward": getattr(pmn_cfg.training_env, "inverse_reward", False),
-#                                     "cost_based": getattr(pmn_cfg.training_env, "cost_based", True),
-#                                     "reward_scale": getattr(pmn_cfg.training_env, "reward_scale", 1.0),
-#                                     "stat_window_size": getattr(pmn_cfg.training_env, "stat_window_size", 5000),}
 
-make_env_parameters = {"graph": getattr(mlp_cfg.training_env, "graph", False),
-                                    "observe_lambda": getattr(mlp_cfg.training_env, "observe_lambda", True),
-                                    "observe_mu": getattr(mlp_cfg.training_env, "observe_mu", True),
+make_env_parameters = {"graph": False,
+                                    "observe_lambda": True,
+                                    "observe_mu": True,
                                     "terminal_backlog": None,
-                                    "observation_keys": getattr(mlp_cfg.training_env, "observation_keys", ["Q", "Y"]),
-                                    "observation_keys_scale": getattr(mlp_cfg.training_env, "observation_keys_scale", None),
-                                    "negative_keys": getattr(mlp_cfg.training_env, "negative_keys", None),
-                                    "symlog_obs": getattr(mlp_cfg.training_env, "symlog_obs", False),
-                                    "symlog_reward": getattr(mlp_cfg.training_env, "symlog_reward", False),
-                                    "inverse_reward": getattr(mlp_cfg.training_env, "inverse_reward", False),
-                                    "cost_based": getattr(mlp_cfg.training_env, "cost_based", True),
-                                    "reward_scale": getattr(mlp_cfg.training_env, "reward_scale", 1.0),
-                                    "stat_window_size": getattr(mlp_cfg.training_env, "stat_window_size", 5000),}
+                                    "observation_keys": ["Q", "Y"],
+                                    "observation_keys_scale": None,
+                                    "negative_keys": None,
+                                    "symlog_obs": True,
+                                    "symlog_reward": False,
+                                    "inverse_reward": True,
+                                    "cost_based": False,
+                                    "reward_scale": 1.0,
+                                    "stat_window_size": 5000}
 
 env_generator = EnvGenerator(context_set, make_env_parameters, env_generator_seed=env_generator_seed)
 base_env = env_generator.sample(0)
@@ -82,67 +83,73 @@ N = int(base_env.base_env.N)
 D = int(input_shape[0]/N)
 
 
-agent_dir = 'n2SH2u_trained_agents'
 
 agent_dict = {}
-# iterate through all agents in agent_dir
-print("Loading agents...")
-for file in os.listdir(agent_dir):
-    if file.endswith(".pt"):
-        # split the file name to get the agent_type and training environment
-        agent_type, env_id = file.split("_")[0], file.split("_")[1]
-        # load either MWN agent or MLP agent based on agent_type
-        if agent_type == "MWN":
-            agent = create_maxweight_actor_critic(
-                input_shape,
-                in_keys=["Q", "Y", "lambda", "mu"],
-                action_spec=base_env.action_spec,
-                temperature=5,
-                init_weights=torch.ones([1, N])
-            )
-        elif agent_type == "MLP":
-            agent = create_actor_critic(
-                input_shape,
-                output_shape,
-                in_keys=["observation"],
-                action_spec=action_spec,
-                temperature=mlp_cfg.agent.temperature,
-                actor_depth=mlp_cfg.agent.hidden_sizes.__len__(),
-                actor_cells=mlp_cfg.agent.hidden_sizes[-1],
-            )
-        elif agent_type == "PMN":
-            agent = create_independent_actor_critic(number_nodes=N,
-                                                    actor_input_dimension=D,
-                                                    actor_in_keys=["Q", "Y", "lambda"],
-                                                    critic_in_keys=["observation"],
-                                                    action_spec=action_spec,
-                                                    temperature=pmn_cfg.agent.temperature,
-                                                    actor_depth=pmn_cfg.agent.hidden_sizes.__len__(),
-                                                    actor_cells=pmn_cfg.agent.hidden_sizes[-1],
-                                                    type=1,
-                                                    network_type="PMN",
-                                                    relu_max=getattr(pmn_cfg, "relu_max", 10), )
+# iterate through all agents in agent types, and get the agents from the trained_agent_folder
+for agent_type in agent_types:
+    # get directory in trained_agent_folder that will share the name with agent_type
+    agent_dir = os.path.join(trained_agent_folder, agent_type)
+    # get the .pt file in the agent_dir, this is the agent's weights
+    agent_file = None
+    for file in os.listdir(agent_dir):
+        if file.endswith(".pt"):
+            agent_file = file
+            break
+    if agent_file is None:
+        continue
+    # load the agent
+    if agent_type == "MWN":
+        agent = create_maxweight_actor_critic(
+            input_shape,
+            in_keys=["Q", "Y", "lambda", "mu"],
+            action_spec=base_env.action_spec,
+            temperature=5,
+            init_weights=torch.ones([1, N])
+        )
+    elif agent_type == "MLP":
+        agent = create_actor_critic(
+            input_shape,
+            output_shape,
+            in_keys=["observation"],
+            action_spec=action_spec,
+            temperature=mlp_cfg.agent.temperature,
+            actor_depth=mlp_cfg.agent.hidden_sizes.__len__(),
+            actor_cells=mlp_cfg.agent.hidden_sizes[-1],
+        )
+    elif agent_type == "PMN":
+        agent = create_independent_actor_critic(number_nodes=N,
+                                                actor_input_dimension=D,
+                                                actor_in_keys=["Q", "Y", "lambda"],
+                                                critic_in_keys=["observation"],
+                                                action_spec=action_spec,
+                                                temperature=pmn_cfg.agent.temperature,
+                                                actor_depth=pmn_cfg.agent.hidden_sizes.__len__(),
+                                                actor_cells=pmn_cfg.agent.hidden_sizes[-1],
+                                                type=3,
+                                                network_type="PMN",
+                                                relu_max=getattr(pmn_cfg, "relu_max", 10), )
 
-        agent.load_state_dict(torch.load(os.path.join(agent_dir, file)))
-        # Store agents
-        agent_dict[(agent_type, env_id)] = agent
+    agent.load_state_dict(torch.load(os.path.join(agent_dir, agent_file)))
+    # Store agents
+    agent_dict[agent_type] = agent
 
 
-# Test each agent on each environment
-lta_tds = {}
-means = {}
-stds = {}
+# Test each agent on each environment, and store the results in the dictionary
+# The results will contain the mean and standard deviation of the lta of the agent on the environment
+results = {key: {} for key in agent_dict.keys()}
+
 context_ids = list(env_generator.context_dicts.keys())
 print("Testing agents...")
 pbar = tqdm(total= len(agent_dict)*num_rollouts*len(context_ids))
 
 with torch.no_grad(), set_exploration_type(ExplorationType.MODE):
     for env_num in context_ids:
-        for (agent_type, training_env), agent in agent_dict.items():
+        for agent_type, agent in agent_dict.items():
             actor = agent.get_policy_operator()
             actor.eval()
             env = env_generator.sample(env_num)
             ltas = []
+            results[agent_type][env_num] = {}
             for n in range(num_rollouts):
                 pbar.set_description(f"Testing agent {agent_type} on environment {env_num}: {n}/{num_rollouts}")
 
@@ -152,14 +159,22 @@ with torch.no_grad(), set_exploration_type(ExplorationType.MODE):
                 pbar.update(1)
             # training_env_num = context_ids[training_env[-1]]
             lta_tds[(env_num, agent_type, env_num)] = torch.stack(ltas)
-            means[(env_num, agent_type, env_num)] = torch.stack(ltas).mean(dim=0)[-1].item()
-            stds[(env_num, agent_type, env_num)] = torch.stack(ltas).std(dim=0)[-1].item()
+            # store the means and std of the ltas in the results dictionary
+            results[agent_type][env_num]["mean"] = torch.stack(ltas).mean(dim=0)[-1].item()
+            results[agent_type][env_num]["std"] = torch.stack(ltas).std(dim=0)[-1].item()
+
             # reset env_generator seed
             env_generator.reseed(env_generator_seed)
 
-results = {"lta_tds": lta_tds, "means": means, "stds": stds}
-with open(f"{agent_dir}/{agent_dir}_results.pkl", "wb") as f:
-    pickle.dump(results, f)
+# now pickle each of the agents results in their respective directories
+for agent_type, agent_results in results.items():
+    with open(f"{trained_agent_folder}/{agent_type}/{agent_type}_results.pkl", "wb") as f:
+        pickle.dump(agent_results, f)
+
+
+# results = {"lta_tds": lta_tds, "means": means, "stds": stds}
+# with open(f"{agent_dir}/{agent_dir}_results.pkl", "wb") as f:
+#     pickle.dump(results, f)
 """ Plot results
 The results keys are (testing_env_id, agent_type, training_env_id)
 We want a single bar plot for each testing_env_id with a bar for each agent_type, training_env_id combination
@@ -167,29 +182,36 @@ Each bar has the mean lta of the agent on the testing environment with error bar
 """
 
 
-
-bar_width = 0.35
-n_environments = len(context_ids)
+# plot the results by iterating through the results dictionary
 
 
-fig, ax = plt.subplots()
-index = np.arange(n_environments)
 
-means_list_pmn = [means[(training_env, "PMN", training_env)] for training_env in context_ids]
-stds_list_pmn = [stds[(training_env, "PMN", training_env)] for training_env in context_ids]
-ax.bar(index - bar_width / 2, means_list_pmn, bar_width, yerr=stds_list_pmn, label="PMN")
 
-means_list_mlp = [means[(training_env, "MLP", training_env)] for training_env in context_ids]
-stds_list_mlp = [stds[(training_env, "MLP", training_env)] for training_env in context_ids]
-ax.bar(index + bar_width / 2, means_list_mlp, bar_width, yerr=stds_list_mlp, label="MLP")
 
-ax.set_xticks(index)
-ax.set_xticklabels(context_ids)
-ax.set_xlabel("Training Environment")
-ax.set_ylabel("Mean LTA")
-ax.set_ylim(0,200)
-ax.legend()
-plt.show()
+
+#
+# bar_width = 0.35
+# n_environments = len(context_ids)
+#
+#
+# fig, ax = plt.subplots()
+# index = np.arange(n_environments)
+#
+# means_list_pmn = [means[(training_env, "PMN", training_env)] for training_env in context_ids]
+# stds_list_pmn = [stds[(training_env, "PMN", training_env)] for training_env in context_ids]
+# ax.bar(index - bar_width / 2, means_list_pmn, bar_width, yerr=stds_list_pmn, label="PMN")
+#
+# means_list_mlp = [means[(training_env, "MLP", training_env)] for training_env in context_ids]
+# stds_list_mlp = [stds[(training_env, "MLP", training_env)] for training_env in context_ids]
+# ax.bar(index + bar_width / 2, means_list_mlp, bar_width, yerr=stds_list_mlp, label="MLP")
+#
+# ax.set_xticks(index)
+# ax.set_xticklabels(context_ids)
+# ax.set_xlabel("Training Environment")
+# ax.set_ylabel("Mean LTA")
+# ax.set_ylim(0,200)
+# ax.legend()
+# plt.show()
 
 # results = {"lta_tds": lta_tds, "means": means, "stds": stds}
 # with open(f"{agent_dir}/{agent_dir}_results.pkl", "wb") as f:
