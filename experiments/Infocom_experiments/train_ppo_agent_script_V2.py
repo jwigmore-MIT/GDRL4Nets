ad028b9d469ea45be1fa9a0e00d8463847009f51
jwigmore-laptop <jwigmore@mit.edu>
2024-07-22 16:46:16 -0400

    For Multipath Testing

--- experiments/Infocom_experiments/train_ppo_agent_script_V2.py
+++ experiments/Infocom_experiments/train_ppo_agent_script_V2.py
@@ -49,8 +49,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run experiment')
     # parser.add_argument('--training_set', type=str, help='indices of the environments to train on', default="b")
     # add training code which will be a tuple of integers (start, number)
-    parser.add_argument('--training_code', type = tuple, help='range of integers to train on', default=(0,1))
-    parser.add_argument('--context_set', type=str, help='reference_to_context_set', default="MP3") # or SH2u
+    parser.add_argument('--training_code', type = tuple, help='range of integers to train on', default=(7,3))
+    parser.add_argument('--context_set', type=str, help='reference_to_context_set', default="MP2") # or SH2u
     # parser.add_argument('--env_params', type=str, help='reference_to_context_set', default="SH1E") # or SH2u
     parser.add_argument('--train_type', type=str, help='base configuration file', default="PMN_shared_PPO_MP")
     parser.add_argument('--cfg', nargs = '+', action='append', type = smart_type, help = 'Modify the cfg object')
